e495cf74679b14ec35fe33b97b835b6747b608be
gholmes829 <g.holmes429@gmail.com>
2020-11-10 00:24:27 -0600

    basic program architecture and reorginization

---
+++ __main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+from driver import Driver
+
+def main(argv, argc):
+	"""
+	Put argv, argc here in case we wanna pass anything in via cmd line
+	"""
+	driver = Driver()
+	driver.run()
+
+if __name__ == "__main__":
+	main(sys.argv, len(sys.argv))
---
+++ driver.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+# import stuff from core here
+#from core.module import ...
+
+class Driver:
+
+	def __init__(self):
+		"""
+		Has attributes like number of oberserved vars and stuf like that
+		"""
+		pass
+
+	def run(self):
+		"""
+		Runs program with pre configured parameters
+		"""
+		pass
