3b30211067f3319e42dab755bc1a5a769fe6753c
gholmes829 <g.holmes429@gmail.com>
2020-11-11 20:17:19 -0600

    function structure

--- core/kalman.py
+++ core/kalman.py
@@ -5,9 +5,22 @@ import numpy as np
 
 class KalmanEnsemble:
 
-	def __init__(self, model, solver, observations):
+	def __init__(self, agents: int, model: np.ndarray, solver: callable, observations: np.ndarray) -> None:
+		self.agents = agents
 		self.model = model
+		self.solver = solver
 		self.observations = observations
-		self.numObservations = sum(self.observations)
-		self.H = np.array([[int(n == m) for n in range(3)] for m in range(3) if observations[m]])
+	
+		self.state = None
+		self.H = np.array([[int(n == m) for n in range(3)] for m in range(3) if observations[m]])	
 
+	def predict(self):
+		pass
+
+	def analyze(self):
+		pass
+
+	@staticmethod
+	def noise(mean: np.ndarray, std: np.ndarray, size: tuple) -> np.ndarray:
+		return np.random.normal(mean, std, size)
+	
